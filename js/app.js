a9d6d05e359e4754c6efbfbeecec6be0f74bf8e7
Juan <juanmataruiz84@gmail.com>
2018-06-22 16:00:52 +0100

    dom working and events created

---
+++ js/app.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+  const animalList = document.querySelector('#animal-list')
+  const form = document.querySelector('#new_item_form')
+
+  const handleFormSave = function(event) {
+    event.preventDefault();
+    const form = event.target;
+    const specie = form.specie.value;
+    const numbers = form.numbers.value;
+    const continent = form.continent.value;
+
+    const result = document.querySelector('#animal-list');
+
+    const paragraph = document.createElement('div');
+    paragraph.textContent = `Specie: ${specie}, Numbers left: ${numbers}, Continent: ${continent}`
+    result.appendChild(paragraph);
+
+    form.reset()
+  }
+
+  form.addEventListener('submit', handleFormSave)
+
+
+});
