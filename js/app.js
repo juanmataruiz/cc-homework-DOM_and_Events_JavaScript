9eb048dfcb23fa2ec688fa5f4019cfd4ad56eb78
Juan <juanmataruiz84@gmail.com>
2018-06-24 15:11:36 +0100

    css added

--- js/app.js
+++ js/app.js
@@ -7,14 +7,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const handleFormSave = function(event) {
     event.preventDefault();
     const form = event.target;
-    const specie = form.specie.value;
+    const species = form.species.value;
     const numbers = form.numbers.value;
     const continent = form.continent.value;
 
     const result = document.querySelector('#animal-list');
 
     const paragraph = document.createElement('div');
-    paragraph.textContent = `Specie: ${specie}, Numbers left: ${numbers}, Continent: ${continent}`
+    paragraph.textContent = `Species: ${species}, Numbers left: ${numbers}, Continent: ${continent}`
     result.appendChild(paragraph);
 
     form.reset()
